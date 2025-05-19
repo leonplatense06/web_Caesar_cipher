dfe01b748ca62b8471ec7e9f4b1a94d88437fb59
Ramiro Lavie <lavieramiro@gmail.com>
2025-05-19 09:25:07 -0300

    encode degree added

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 const input = document.getElementById("input");
 const result = document.getElementById("result");
+const degree = document.getElementById("slider");
+const sliderValue = document.getElementById("slider-value");
 
 function encode() {
     let charList = input.value.split("");
+    let encodeDegree = parseInt(degree.value);
     for (let i=0;i < charList.length;i++){
-        charList[i] = String.fromCharCode(charList[i].charCodeAt(0) + 1);
+        charList[i] = String.fromCharCode(charList[i].charCodeAt(0) + encodeDegree);
     }
     result.textContent = charList.join("");
+}
+
+function updateValue() {
+    sliderValue.textContent = degree.value;
 }
